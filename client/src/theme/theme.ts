0069d9461964a33b8c694b65b0de272852768fcc
João Gularte <107907952+OndeJoaoGularte@users.noreply.github.com>
2026-01-04 19:40:52 -0300

    att fonte

--- client/src/theme/theme.ts
+++ client/src/theme/theme.ts
@@ -1,37 +1,37 @@
-import { createTheme, type ThemeOptions } from '@mui/material/styles';
+import { createTheme, type ThemeOptions } from "@mui/material/styles";
 
 export const themeOptions: ThemeOptions = {
   palette: {
-    mode: 'dark',
+    mode: "dark",
     text: {
-      primary: '#ffffff',
+      primary: "#ffffff",
     },
     primary: {
-      main: '#7F00FF',
+      main: "#7F00FF",
     },
     secondary: {
-      main: '#FFFF7F',
+      main: "#FFFF7F",
     },
     background: {
-      default: '#121212',
-      paper: '#121212',
+      default: "#121212",
+      paper: "#121212",
     },
     error: {
-      main: '#ff1000',
+      main: "#ff1000",
     },
     warning: {
-      main: '#FF7F00',
+      main: "#FF7F00",
     },
     info: {
-      main: '#007FFF',
+      main: "#007FFF",
     },
     success: {
-      main: '#00FF7F',
+      main: "#00FF7F",
     },
-    divider: 'rgba(255,255,255,0.12)',
+    divider: "rgba(255,255,255,0.12)",
   },
   typography: {
-    fontFamily: 'Source Sans Pro',
+    fontFamily: '"Source Sans 3", sans-serif',
     fontWeightBold: 700,
     htmlFontSize: 16,
     fontWeightMedium: 500,
@@ -39,20 +39,20 @@ export const themeOptions: ThemeOptions = {
     fontWeightLight: 300,
     fontSize: 14,
     h1: {
-      fontFamily: 'Source Sans Pro',
-      fontSize: '6rem',
+      fontFamily: '"Source Sans 3", sans-serif',
+      fontSize: "6rem",
       fontWeight: 300,
       lineHeight: 1.167,
     },
     h2: {
-      fontFamily: 'Source Sans Pro',
-      fontSize: '3.75rem',
+      fontFamily: '"Source Sans 3", sans-serif',
+      fontSize: "3.75rem",
       fontWeight: 300,
       lineHeight: 1.2,
     },
     h3: {
-      fontFamily: 'Source Sans Pro',
-      fontSize: '3rem',
+      fontFamily: '"Source Sans 3", sans-serif',
+      fontSize: "3rem",
       fontWeight: 400,
       lineHeight: 1.167,
     },
